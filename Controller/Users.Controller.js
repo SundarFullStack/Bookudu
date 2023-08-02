c15ada53d12de55ac14a4132c59283d9abfeeebd
Sundar Thambidurai <meenakshisunder183017@gmail.com>
2023-08-02 11:48:21 +0530

    Third Commit

--- Controller/Users.Controller.js
+++ Controller/Users.Controller.js
@@ -1,5 +1,6 @@
-const UserRouter = require('express').Router();
-const UserModel = require('../Models/Users.model');
+const UserRouter = require("express").Router();
+const UserModel = require("../Models/Users.model");
+const mongoose = require("express");
 
 //GET ALL THE USERS
 /**
@@ -8,30 +9,31 @@ const UserModel = require('../Models/Users.model');
  * RESPONSE - ARRAY<USERS>
  */
 
-UserRouter.get('/',(request,response,next)=>{
-
-  UserModel.find().then((cursor)=>{
-    if(cursor && cursor.length >0){
-      return response.status(200).json({
-        success:true,
-        data:cursor,
-        message:"ALL USERS RETREIVED SUCCESSFULLY!!!",
-      })
-    }else{
-         return response.status(200).json({
-        success:true,
-        data:[],
-        message:"USERS COLLECTION IS EMPTY!!!",
-         })
-    }
-  }).catch((err)=>{
-    return response.status(401).json({
-      success:false,
-      message:"ERROR IN FETCHING DATA",
-      err:err,
+UserRouter.get("/", (request, response, next) => {
+  UserModel.find()
+    .then((cursor) => {
+      if (cursor && cursor.length > 0) {
+        return response.status(200).json({
+          success: true,
+          data: cursor,
+          message: "ALL USERS RETREIVED SUCCESSFULLY!!!",
+        });
+      } else {
+        return response.status(200).json({
+          success: true,
+          data: [],
+          message: "USERS COLLECTION IS EMPTY!!!",
+        });
+      }
     })
-  })
-})
+    .catch((err) => {
+      return response.status(401).json({
+        success: false,
+        message: "ERROR IN FETCHING DATA",
+        err: err,
+      });
+    });
+});
 
 //CREATE A USER
 /**
@@ -40,28 +42,61 @@ UserRouter.get('/',(request,response,next)=>{
  * RESPONSE - OBJECT<USER> + POST RESPONSE STATUS
  */
 
-UserRouter.post('/create',(request,response,next)=>{
-
+UserRouter.post("/create", (request, response, next) => {
   const data = request.body;
   // console.log(data);
   const User = new UserModel(data);
   // console.log("user",User);
-  User.save().then((result)=>{
-    // console.log(result);
-    if(result && result._id){
-      return response.status(200).json({
-        success:true,
-        message:"USER CREATED SUCCESSFULLY",
-        data:result,
-      })
-    }
-  }).catch((err)=>{
-     return response.status(401).json({
-        success:false,
-        message:"ERROR IN CREATING USER",
-        error:err,
-      })
-  })
-})
+  User.save()
+    .then((result) => {
+      // console.log(result);
+      if (result && result._id) {
+        return response.status(200).json({
+          success: true,
+          message: "USER CREATED SUCCESSFULLY",
+          data: result,
+        });
+      }
+    })
+    .catch((err) => {
+      return response.status(401).json({
+        success: false,
+        message: "ERROR IN CREATING USER",
+        error: err,
+      });
+    });
+});
 
-module.exports = UserRouter
+//UPDATE THE USERS USING PATCH METHOD
+/**
+ * METHOD- PATCH
+ * REQUEST - OBJECT<USER>
+ * RESPONSE - OBJECT<UPDATEDUSER>
+ */
+
+UserRouter.patch("/:id", (request, response, next) => {
+  const { id } = request.params;
+  const updateData = request.body;
+  // console.log(id);
+  // console.log(data);
+  UserModel.findOneAndUpdate({ _id: id }, { $set: updateData }, { new: true })
+    .then((result) => {
+      // console.log(result);
+      if (result && result._id) {
+        return response.status(200).json({
+          success: true,
+          message: "USER UPDATED SUCCESSFULLY",
+          UpdatedData: result,
+        });
+      }
+    })
+    .catch((err) => {
+      return response.status(401).json({
+        success: false,
+        message: "ERROR IN UPDATING USER",
+        error: err,
+      });
+    });
+});
+
+module.exports = UserRouter;
